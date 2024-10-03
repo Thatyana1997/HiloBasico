7764e2b81f2df9f0821996ef10e0d8fd7c1beb59
KarlaBC <karlatataianabc@gmail.com>
2024-10-02 21:00:38 -0600

    funcion del hilo

--- HiloBasico/Program.cs
+++ HiloBasico/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HiloBasico
@@ -10,6 +11,28 @@ namespace HiloBasico
     {
         static void Main(string[] args)
         {
+            Thread hilo1 = new Thread(FuncionHilo);
+            Thread hilo2 = new Thread(FuncionHilo2);
+
+            hilo1.Start();
+            hilo2.Start();
+        }
+
+        public static void FuncionHilo()
+        {
+            for(int i=0; i<10;i++)
+            {
+                Console.WriteLine("Hilo principal "+i);
+                Thread.Sleep(500);
+            }
+        }
+        public static void FuncionHilo2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hilo secundario " + i);
+                Thread.Sleep(500);
+            }
         }
     }
 }
